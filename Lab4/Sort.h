461dcafe270aa347687d5d244954864778339253
Stanulet Sebastian <vexorwarlock@gmail.com>
2025-04-07 20:53:34 +0300

    Create Sort.h

---
+++ Lab4/Sort.h
@@ -0,0 +1,12 @@
+#pragma once
+
+class Sort
+{
+public:
+    void InsertSort(bool ascendent=false);
+    void QuickSort(bool ascendent=false);
+    void BubbleSort(bool ascendent=false);
+    void Print();
+    int  GetElementsCount();
+    int  GetElementFromIndex(int index);
+};
