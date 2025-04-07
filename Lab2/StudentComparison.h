6eadeb74bddb7e1828f640bab61691e27f2035a0
Stanulet Sebastian <vexorwarlock@gmail.com>
2025-04-07 20:29:50 +0300

    Create StudentComparison.h

---
+++ Lab2/StudentComparison.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "Student.h"
+
+int compareByName(const Student& s1, const Student& s2);
+int compareByMath(const Student& s1, const Student& s2);
+int compareByEnglish(const Student& s1, const Student& s2);
+int compareByHistory(const Student& s1, const Student& s2);
+int compareByAverage(const Student& s1, const Student& s2);
