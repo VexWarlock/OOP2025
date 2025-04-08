ebeaf8a7957a3fd2ab478d53c16bfe1b6cd45747
Stanulet Sebastian <vexorwarlock@gmail.com>
2025-04-08 18:05:03 +0300

    Update main.cpp

--- Lab7/main.cpp
+++ Lab7/main.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+using namespace std;
 
+constexpr long double operator"" _Kelvin(long double kelvin) {
+    return kelvin - 273.15;
+}
+
+constexpr long double operator"" _Fahrenheit(long double fahrenheit) {
+    return (fahrenheit - 32) * 5.0 / 9.0;
+}
+
+int main() 
+{
+    
+float a = 300.0_Kelvin;
+float b = 120.0_Fahrenheit;
+  
+cout << "Kelvin to Celsius: " << a << "°C\n";
+cout << "Fahrenheit to Celsius: " << b << "°C\n";
+
+}
