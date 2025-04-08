214ad1890f0c27f3c958fe745f9e818ae2f03467
Stanulet Sebastian <vexorwarlock@gmail.com>
2025-04-08 03:19:53 +0300

    Update Sort.h

--- Lab4/Sort.h
+++ Lab4/Sort.h
@@ -1,12 +1,30 @@
 #pragma once
+#include <initializer_list>
 
 class Sort
 {
+private:
+    int* arr;
+    int size;
+
 public:
-    void InsertSort(bool ascendent=false);
-    void QuickSort(bool ascendent=false);
-    void BubbleSort(bool ascendent=false);
+    // Constructors
+    Sort(int numElements, int min, int max);
+    Sort(int count, int* values);
+    Sort(const char* numbers);
+    Sort(std::initializer_list<int> list);
+    Sort(int count, ...);
+
+    // Destructor
+    ~Sort();
+
+    // Sorting methods
+    void InsertSort(bool ascendent = false);
+    void QuickSort(bool ascendent = false);
+    void BubbleSort(bool ascendent = false);
+
+    // Utility methods
     void Print();
-    int  GetElementsCount();
-    int  GetElementFromIndex(int index);
+    int GetElementsCount();
+    int GetElementFromIndex(int index);
 };
